d8d9d78ebb57fa63ee16d16f949f47fca5765d01
Braz <anderson.braz@sensedia.com>
2017-07-04 08:24:26 -0300

    Rest Client Mock

---
+++ src/main/java/com/sensedia/test/app/RuntimeEnvironment.java
@@ -0,0 +1,15 @@
+package com.sensedia.test.app;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Profile;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Profile({ "default", "dev", "staging", "homolog", "production" })
+public @interface RuntimeEnvironment {
+
+}
---
+++ src/main/java/com/sensedia/test/infra/RestClientMock.java
@@ -0,0 +1,29 @@
+package com.sensedia.test.infra;
+
+import org.springframework.http.ResponseEntity;
+
+import com.sensedia.api.infra.RestClient.Limit;
+import com.sensedia.api.infra.RestClient.Offset;
+import com.sensedia.api.infra.RestClient.PagedOperation;
+import com.sensedia.api.infra.RestClient.ResponseHandler;
+
+public class RestClientMock {
+	public <T> PagedOperation<T> pagedOperation(ResponseEntity<T> response) {
+		return new PagedOperationMock<>(response);
+	}
+
+	static class PagedOperationMock<R> extends PagedOperation<R> {
+		private ResponseEntity<R> response;
+
+		public PagedOperationMock(ResponseEntity<R> response) {
+			super("mock://test", Limit.of(1), Offset.of(1));
+			this.response = response;
+		}
+
+		@Override
+		public PagedOperation<R> onResponse(ResponseHandler<R> handler, Class<R> type) {
+			handler.onResponse(this.response);
+			return this;
+		}
+	}
+}
