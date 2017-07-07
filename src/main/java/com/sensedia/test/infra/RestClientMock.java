ec9f8af1d6f63edc7cf818a5832c8052eff586b5
Braz <anderson.braz@sensedia.com>
2017-07-07 13:49:25 -0300

    Rest Get Operation Mock

--- src/main/java/com/sensedia/test/infra/RestClientMock.java
+++ src/main/java/com/sensedia/test/infra/RestClientMock.java
@@ -1,15 +1,36 @@
 package com.sensedia.test.infra;
 
+import java.util.HashMap;
+
 import org.springframework.http.ResponseEntity;
 
+import com.sensedia.api.infra.RestClient.GetOperation;
 import com.sensedia.api.infra.RestClient.Limit;
 import com.sensedia.api.infra.RestClient.Offset;
 import com.sensedia.api.infra.RestClient.PagedOperation;
 import com.sensedia.api.infra.RestClient.ResponseHandler;
 
 public class RestClientMock {
+	public static <T> GetOperation<T> getOperation(ResponseEntity<T> response) {
+		return new GetOperationMock<>(response);
+	}
+	
 	public static <T> PagedOperation<T> pagedOperation(ResponseEntity<T> response) {
 		return new PagedOperationMock<>(response);
+	}
+	
+	static class GetOperationMock<R> extends GetOperation<R> {
+		private ResponseEntity<R> response;
+
+		public GetOperationMock(ResponseEntity<R> response) {
+			super("mock://test", new HashMap<>());
+			this.response = response;
+		}
+
+		@Override
+		public void onResponse(ResponseHandler<R> handler, Class<R> type) {
+			handler.onResponse(this.response);
+		}
 	}
 
 	static class PagedOperationMock<R> extends PagedOperation<R> {
@@ -21,9 +42,8 @@ public class RestClientMock {
 		}
 
 		@Override
-		public PagedOperation<R> onResponse(ResponseHandler<R> handler, Class<R> type) {
+		public void onResponse(ResponseHandler<R> handler, Class<R> type) {
 			handler.onResponse(this.response);
-			return this;
 		}
 	}
 }
