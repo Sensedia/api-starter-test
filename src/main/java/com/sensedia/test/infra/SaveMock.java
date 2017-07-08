426f310603ddc089c4fdd2c56572eb950b4c7186
Braz <anderson.braz@sensedia.com>
2017-07-08 09:58:57 -0300

    -SaveMock by field

--- src/main/java/com/sensedia/test/infra/SaveMock.java
+++ src/main/java/com/sensedia/test/infra/SaveMock.java
@@ -1,11 +1,33 @@
 package com.sensedia.test.infra;
 
+import java.lang.reflect.Field;
+
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 public class SaveMock<T> implements Answer<T> { 
+	private final String idColumnName;
+	
+	private final Object value;
+	
+	public SaveMock(String idColumnName, Object value) {
+		this.idColumnName = idColumnName;
+		this.value = value;
+	}
+	
+	public SaveMock() {
+		this.idColumnName = "id";
+		this.value = 1L;
+	}
+
 	@Override
 	public T answer(InvocationOnMock invocation) throws Throwable {
-		return (T) invocation.getArgumentAt(0, Object.class);
+		Object result = invocation.getArgumentAt(0, Object.class);
+		
+		Field id = result.getClass().getDeclaredField(this.idColumnName);
+		id.setAccessible(true);
+		id.set(result, this.value);
+				
+		return (T) result;
 	}
 }
